11202035ce73e72f8607cbcf45fdcd5bbce897a0
Jonathan Serafini <jonathan@serafini.ca>
2016-07-07 14:08:28 -0400

    Break compatibility of include_recipes, require hash

--- recipes/install_modules.rb
+++ recipes/install_modules.rb
@@ -1,5 +1,6 @@
 # Include additional recipes, modules, overrides, etc...
 #
-node['nginx_resources']['source']['include_recipes'].each do |recipe|
+node['nginx_resources']['source']['include_recipes'].each do |recipe, bool|
+  next unless bool
   include_recipe recipe
 end
